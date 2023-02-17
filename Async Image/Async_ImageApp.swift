f2e01e4abd99f587357486c50279a0347f810ab3
Umar Amjad <mirza.umar.amjad@gmail.com>
2023-02-17 03:58:38 -0500

    Initial Commit

---
+++ Async Image/Async_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_ImageApp.swift
+//  Async Image
+//
+//  Created by Umar Amjad on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Async_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
